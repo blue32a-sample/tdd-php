423ae04884e530d66c8f2be60d1add52bfb97f65
blue32a <blue32a.jp@gmail.com>
2021-09-25 22:02:50 +0900

    第16章 将来の読み手を考えたテスト (Abstraction, Finally)
    第1部 多国通貨 (The Money Example)

---
+++ src/Part01TheMoneyExample/Ch16AbstractionFinally/Bank.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TDDPHP\Part01TheMoneyExample\Ch16AbstractionFinally;
+
+class Bank
+{
+    /** @var array<Pair,int> $rates */
+    private array $rates = [];
+
+    public function reduce(Expression $source, string $to): Money
+    {
+        return $source->reduce($this, $to);
+    }
+
+    public function addRate(string $from, string $to, int $rate)
+    {
+        $this->rates[(new Pair($from, $to))->hashCode()] = $rate;
+    }
+
+    public function rate(string $from, string $to): int
+    {
+        if ($from === $to) {
+            return 1;
+        }
+        return $this->rates[(new Pair($from, $to))->hashCode()];
+    }
+}
